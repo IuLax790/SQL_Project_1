cd617688e818fdc19ff1dde0b2c26b464fb89d60
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2022-12-08 13:12:40 +0200

    Add files via upload

---
+++ Ido_Koubi_Querry.sql
@@ -0,0 +1,11 @@
+﻿use Company
+
+SELECT * from Products
+SELECT orders.OrderID ,orders.ProductID,Customers.CustomerId,Customers.FirstName,
+Customers.LastName,
+Products.ProductName,  cast(Products.UnitPrice as varchar)+'$' as 'Price per unit'  ,cast(Freight as varchar)+'$' as Shipping_cost,ShipAddress,ShipCity,ShipCountry from orders join Products ON orders.ProductID = Products.ProductID 
+join Customers on orders.CustomerID= Customers.CustomerID
+
+SELECT Shippers.OrderID, orders.ShipCity,orders.ShipCountry,orders.ShipAddress,Shippers.ShippingTime,shippers.TrackingNumber 
+from Shippers 
+join orders on Shippers.OrderID = orders.OrderID
